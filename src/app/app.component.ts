aca0c32b0b3bebca5ebb082da2f93501c6bbc5ca
Gleidson <gleidsonrosasantos2@gmail.com>
2026-07-29 16:47:56 -0300

    depois explico kkkkkkkkkkkkkkkk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaProdutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
